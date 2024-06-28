1f239659910e5d707f84e9fc1619c61a79e9d470
nathaniEL mujesia <nathanielmujesia@gmail.com>
2024-06-28 13:46:10 +0300

    feat: built darkmode component

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,6 +1,7 @@
 const Login = () => {
   return (
-    <div>Login</div>
+
+      <p>Login</p>
   )
 }
 
--- src/components/DarkMode/useDarkMode.jsx
+++ src/components/DarkMode/useDarkMode.jsx
@@ -1,6 +1,20 @@
+import { useState,useEffect } from "react"
+
 const useDarkMode = () => {
+const [isDarkMode, setIsDarkMode] = useState(() => {
+  const savedMode = localStorage.getItem('dark-mode')
+  return savedMode ? JSON.parse(savedMode) : false
+});
+
+useEffect(()=>{
+  isDarkMode ? document.documentElement.classList.add('dark') : document.documentElement.classList.remove('dark')
+  localStorage.setItem('dark-mode',JSON.stringify(isDarkMode))
+},[isDarkMode])
+
+  const toggleDarkMode = () => setIsDarkMode(!isDarkMode)
+
   return (
-    <div>useDarkMode</div>
+    [isDarkMode,toggleDarkMode]
   )
 }
 
