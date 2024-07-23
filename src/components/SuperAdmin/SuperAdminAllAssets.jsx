49505284064567aefd26d850feb99e1f378b0a9f
nathaniEL mujesia <nathanielmujesia@gmail.com>
2024-07-23 22:18:21 +0300

    feat: super-admin all assets  page search and filter f(x) added

--- src/components/SuperAdmin/SuperAdminAllAssets.jsx
+++ src/components/SuperAdmin/SuperAdminAllAssets.jsx
@@ -1,18 +1,103 @@
 import SuperAdminAllAssetsCard from "./SuperAdminAllAssetsCard"
+import api from "../../API/BaseURL"
+import { addFilterBtns } from "../SharedComponents/SharedFunctions"
+
+import { useEffect, useReducer, useState } from "react" 
+
+const superAdminAllAssetsReducer = (state,action) =>{
+  switch(action.type){
+    case"search":
+      return {...state,search:action.payload} 
+    case"category":          
+        return {...state,category:[...action.payload]}
+    default:
+      throw new Error('action type is undefined') 
+  }
+}
+
 
 
 const SuperAdminAllAssets = () => {
+  
+  const [state,dispatch] = useReducer(superAdminAllAssetsReducer,{
+                                                                search:"",
+                                                                category:[]
+  })
+  const [assets,setAssets] = useState([])
+  const [selectedCategories,setSelectedCategories] = useState([])
+  const [displayedAssets,setDisplayedAssets] = useState([])
+
+    useEffect( () =>{
+    const fetchAssets = async () =>{
+      try{
+        const resp = await api.get("assets/allassets/")
+        setAssets(resp.data)
+        dispatch({type:"category",payload:addFilterBtns(resp.data)})
+        setDisplayedAssets(resp.data)
+      }
+      catch(err){
+        console.error("Error fetching data: ",err)
+      }
+    }
+    fetchAssets()
+  },[])
+
+
+  const handleCategorySelection = (category) =>{
+    let categoryToLowerCase = category.toLowerCase()
+    const newSelectedCategories = selectedCategories.includes(categoryToLowerCase)
+      ? selectedCategories.filter((el) => el !== categoryToLowerCase)
+      : [...selectedCategories, categoryToLowerCase];
+
+    setSelectedCategories(newSelectedCategories);
+    filterDisplay(newSelectedCategories);
+  }
+
+  const filterDisplay = (categories) => {
+      if(categories.length === 0 ){        
+        setDisplayedAssets(assets)
+      }
+      else{           
+        const filteredAssets = assets.filter((asset) =>
+        categories.includes(asset.category.toLowerCase())
+      );
+      setDisplayedAssets(filteredAssets)        
+  }
+  }
+
   return (
-    <div className=" super-admin-all-assets-pg | flow">
-      <SuperAdminAllAssetsCard/>
-      <SuperAdminAllAssetsCard/>
-      <SuperAdminAllAssetsCard/>
-      <SuperAdminAllAssetsCard/>
-      <SuperAdminAllAssetsCard/>
-      <SuperAdminAllAssetsCard/>
-      <SuperAdminAllAssetsCard/>
-      <SuperAdminAllAssetsCard/>
-    </div>
+      <div className="super-admin-all-assets">
+        <div className="container">
+          <header className="super-admin-all-assets-header |  dark:text-neutral-200">
+            <div className="super-admin-total-assets-and-assets-search-wrapper | flex">
+              <div className="super-admin-all-assets-filtered-assets-wrapper | flex ">
+                <p><span className="uppercase font-bold">all assets</span>: <span
+                    className="font-semibold">{assets.length}</span></p>
+                <p><span className="uppercase font-bold ">filtered assets</span>: <span
+                    className="font-semibold">{displayedAssets.length}</span></p>
+              </div>
+              <div className="super-admin-all-assets-search-bar-wrapper">
+                <input onChange={e=> dispatch({type:"search",payload:e.target.value})}type="text"
+                className="super-admin-all-assets-search-bar" placeholder="Search by name or type" />
+              </div>
+            </div>
+            <div className="super-admin-all-assets-filter-btns  | flex">
+              {state.category.map((name)=>{
+              return (<button onClick={()=>{handleCategorySelection(name)}}
+                className={`capitalize btn ${selectedCategories.includes(name)?"filter-primary-btn":"ghost-btn"}`}
+                key={name}>{name}
+              </button>)
+              })}</div>
+          </header>
+        </div>
+        <div className="super-admin-all-assets-body flow">
+          {displayedAssets.filter((asset) => {
+          return (state.search.toLowerCase() === '' ? asset : asset.name.toLowerCase().includes(state.search) ||
+          asset.asset_type.toLowerCase().includes(state.search) )
+          }).map(asset =>
+          <SuperAdminAllAssetsCard asset={asset} key={asset.id} />)}
+        </div>
+      </div>
   )
 }
 
--- src/components/SuperAdmin/SuperAdminAllAssetsCard.jsx
+++ src/components/SuperAdmin/SuperAdminAllAssetsCard.jsx
@@ -1,7 +1,9 @@
-import { NavLink } from "react-router-dom"
+import { Link } from "react-router-dom"
+import PropTypes from 'prop-types';  
 
-const SuperAdminAllAssetsCard = () => {
-  return (
+const SuperAdminAllAssetsCard = ({asset}) => {
+    const {id,name,category,serial_number,tag,asset_type} = asset
+    return (
         <div className="md:super-admin-all-assets-card-md sm:super-admin-all-assets-card-sm | shadow grid bg-neutral-100 dark:bg-neutral-600 dark:text-neutral-200">
         {/* Card Img */}
         <img className="super-admin-all-assets-card-img"
@@ -9,20 +11,23 @@ const SuperAdminAllAssetsCard = () => {
             alt="unsplash-radom" />
         {/* Card Details */}
         <div className="super-admin-all-assets-card-info | flow">
-            <p className="capitalize super-admin-all-assets-card-info-p"><span className="font-semibold">name:</span> HP Spectre x360
-                2-in-1 Laptop 14t-eu000, 14</p>
-            <p className="capitalize super-admin-all-assets-card-info-p"><span className="font-semibold">serial:</span> 0000000000</p>
-            <p className="capitalize super-admin-all-assets-card-info-p"><span className="font-semibold">tag no:</span> 0000000000</p>
-            <p className="capitalize super-admin-all-assets-card-info-p"><span className="font-semibold">category:</span> electronics</p>
-            <p className="capitalize super-admin-all-assets-card-info-p"><span className="font-semibold">type:</span> laptop</p>
+            <p className="capitalize super-admin-all-assets-card-info-p"><span className="font-semibold">name:</span> {name}</p>
+            <p className="capitalize super-admin-all-assets-card-info-p"><span className="font-semibold">serial:</span> {serial_number}</p>
+            <p className="capitalize super-admin-all-assets-card-info-p"><span className="font-semibold">tag no:</span> {tag}</p>
+            <p className="capitalize super-admin-all-assets-card-info-p"><span className="font-semibold">category:</span> {category}</p>
+            <p className="capitalize super-admin-all-assets-card-info-p"><span className="font-semibold">type:</span> {asset_type}</p>
             {/* Card Btn */}
             <div className="sm:super-admin-all-assets-card-btns-wrapper-sm md:super-admin-all-assets-card-btns-wrapper-md super-admin-all-assets-card-btns-wrapper | flex">
                 <button className="btn primary-btn uppercase text-300 font-bold">delete</button>
-                <NavLink className="link capitalize" to="#">details</NavLink>
+                <Link className="link capitalize" to={`assets/${id}`}>details</Link>
             </div>
         </div>
     </div>
-  )
+    )
 }
 
 export default SuperAdminAllAssetsCard
+
+SuperAdminAllAssetsCard.propTypes = {
+    asset:PropTypes.object
+}
